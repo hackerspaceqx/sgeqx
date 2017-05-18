b11c076d2fe4dbc9c6f85ebd48b19924a1af22bf
brianmviana <brianmviana@alu.ufc.br>
2017-05-18 09:06:24 -0300

    Spring Projeto Criado

---
+++ src/main/java/org/hackerspaceqx/sgeqx/SgeqxApplication.java
@@ -0,0 +1,12 @@
+package org.hackerspaceqx.sgeqx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgeqxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgeqxApplication.class, args);
+	}
+}
